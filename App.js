1f71ad2ecb3ce6c569ba9234a4a7de85f1e2bd3f
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-07-22 14:53:43 +0530

    Added working withstate and events outputting a list of items

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const  [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler=(enteredText) =>{
+    setEnteredGoal(enteredText);
+  };
+
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]); 
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}  >
         <TextInput
           placeholder='Course Goal'
-          style={styles.input} />
-        <Button title="ADD" />
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
 
       <View>
-
+      {courseGoals.map((goal) => <View  key={goal} style={styles.listItems} ><Text>{goal}</Text></View>)}
       </View>
 
     </View>
@@ -25,5 +39,12 @@ const styles = StyleSheet.create({
     padding: 50
   },
   inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }
-  , input: { width: "80%", borderColor: 'black', borderWidth: 1 , padding: 10}
+  , input: { width: "80%", borderColor: 'black', borderWidth: 1 , padding: 10},
+  listItems: {
+  padding: 10,
+  marginVertical: 10,
+  backgroundColor: '#ccc',
+  borderColor: 'black',
+  borderWidth: 1
+  }
 });
