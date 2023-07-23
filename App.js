3127e31af4c2ccc312a297492435a93b10aacf1e
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-07-23 23:11:20 +0530

    Added GoalInput for passing data between components

--- App.js
+++ App.js
@@ -2,30 +2,22 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+ 
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
+  
 
 
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, {id : Math.random().toString(), value: enteredGoal}]);
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, {id : Math.random().toString(), value: goalTitle }]);
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}  >
-        <TextInput
-          placeholder='Course Goal'
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal} />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View> 
+       <GoalInput onAddGoal={addGoalHandler}/>
 
       <FlatList 
       keyExtractor={(item, index) => item.id}
@@ -39,7 +31,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 50
   },
-  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }
-  , input: { width: "80%", borderColor: 'black', borderWidth: 1, padding: 10 },
+ 
  
 });
